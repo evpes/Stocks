e2037767aea0ff70b8813860199031abad6ad00c
Evgeny Pesterev <apsterio.anima@gmail.com>
2022-07-10 14:54:44 +0500

    feat: add titleView

---
+++ Stocks/Other/Extensions.swift
@@ -0,0 +1,37 @@
+//
+//  Extensions.swift
+//  Stocks
+//
+//  Created by evpes on 10.07.2022.
+//
+
+import Foundation
+import UIKit
+
+// MARK: - Framing
+
+extension UIView {
+    var width: CGFloat {
+        frame.size.width
+    }
+    
+    var height: CGFloat {
+        frame.size.height
+    }
+    
+    var left: CGFloat {
+        frame.origin.x
+    }
+    
+    var right: CGFloat {
+        left + width
+    }
+    
+    var top: CGFloat {
+        frame.origin.y
+    }
+    
+    var bottom: CGFloat {
+        top + height
+    }
+}
